3648fef90dca15202aeb406b1fe3b73512886bf8
jhb96 <jhb96@users.noreply.github.com>
2018-05-23 20:20:44 +0200

    V1SinPlay

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -153,3 +153,81 @@ exports.check = (req, res, next) => {
         answer
     });
 };
+/*Metodo randomplay
+/Genera pregunta al azar y cuenta el número de aciertos
+/consecutivos.
+/Esta función se llama cuando se clickea en Play
+*/
+
+exports.randomplay = (req, res, next) => {
+    let score = 0;
+     
+     //Si req.session.randomplay es indefinido 
+     if(req.session.randomplay === undefined){
+        req.session.randomplay = req.session.randomplay || [];
+     };
+
+     models.quiz.findAll({where: {id:{[Sequelize.Op.notIn]: req.session.randomplay}}})
+     .then(quizzes =>{ 
+
+        //Entre todos los quizzes elegimos uno aleatorio
+        let quiz = quizzes[Math.floor(Math.random() * quizzes.length)];
+        //El score será la longitud de req.session.randomplay, ya que contiene
+        //las preguntas respondidas correctamente
+        let score = req.session.randomplay.length || 0;
+        
+        //Si no hay preguntas  req.session.randomplay lo vaciamos y
+        // devolvemos el score como res.
+        if(quizzes.length === 0){ 
+            req.session.randomplay = [];
+            res.render('quizzes/random_nomore',{score});
+            return;
+        }    
+        //Devolvemos el score y la pregunta como res
+        res.render('quizzes/random_play', {score,quiz});
+    })
+
+
+     .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');        
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('quizzes/random_play', {score, quiz});
+    })
+    .catch(error => {
+        req.flash('error', 'Error playing the Quiz: ' + error.message);
+        next(error);
+   });
+};
+
+exports.randomCheck = (req, res, next) =>{
+
+    if(req.session.randomplay === undefined){
+        req.session.randomplay = req.session.randomplay || [];
+     };
+
+    const {quiz, query} = req;
+
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+    let score;
+    //Si acierta la pregunta, el score se actualiza al valor de la long del array de 
+    //acertadas
+    //Se añade a este el valor de la quiz acertada
+    if(result) {
+        req.session.randomplay.push(quiz.id);
+        score = req.session.randomplay.length;
+    //sino, el score se queda en el actual y la el array de acertadas
+    //se pone a cero
+    }else{
+        score = req.session.randomplay.length;
+        req.session.randomplay = [];
+    }
+
+    //Finalmente, se devuelve en res el score, la respuesta
+    //y el resultado y quizzes/random_result lo resuelve
+    res.render('quizzes/random_result',{
+        answer,
+        result,
+        score
+    });
+};
